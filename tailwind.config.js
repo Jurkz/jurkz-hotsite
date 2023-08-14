c1cc964cb758e6d6d3a51f1c377d38f7fb366b63
Vinicius Klinger <viniciusklinger@yahoo.com.br>
2023-08-14 20:07:25 -0300

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html"],
   theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+    fontFamily: {
+      'raleway': ['Raleway', 'sans-serif'],
+      'cabin': ['Cabin', 'sans-serif'],
+    },
     extend: {},
   },
-  plugins: [require("daisyui")],
 }
 
